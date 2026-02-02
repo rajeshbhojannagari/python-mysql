f3c0effc66624ae20d8dff447188891b3007a044
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-02-02 11:47:20 +0530

    inserted values into db using python

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ con=connector.connect(
     password="Rajesh@2005",
     database="python_sql_connection"
 )
-
 print("connection established successfully")
 
+mycursor=con.cursor()
+# mycursor.execute("SHOW DATABASES")
 
+# for x in mycursor:
+#     print(x)
+# created table named Customers
+# mycursor.execute("CREATE TABLE Customers (name VARCHAR(255), address VARCHAR(255))")
+
+# inserting details in customers table
+
+sql="INSERT INTO Customers (name,address) VALUES (%s,%s)"
+val=("Rajesh","Hyderabad")
+mycursor.execute(sql,val)
+con.commit()
+print(mycursor.rowcount,"record inserted")
