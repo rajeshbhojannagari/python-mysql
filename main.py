714087b174d05d7ce1d88bfa78f3fde84bae8c21
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-02-02 11:30:54 +0530

    connecting python to sql

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import mysql.connector as connector
 print("MySQl Connector imported successfully")
+
+
+con=connector.connect(
+    host="localhost",
+    user="root",
+    password="Rajesh@2005",
+    database="python_sql_connection"
+)
+
+print("connection established successfully")
+
+
